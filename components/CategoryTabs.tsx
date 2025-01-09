e08749c9349a04d6e01b5ff4dd843d06e1dc89e2
Ram Goel <rgoel766@gmail.com>
2025-01-09 17:54:34 +0530

    feat: tabs design update

--- components/CategoryTabs.tsx
+++ components/CategoryTabs.tsx
@@ -9,18 +9,27 @@ const CategoryTabs = ({
 }) => {
   const { allTasks } = useAppState();
   return (
-    <TabsList className="w-fit">
-      <TabsTrigger onClick={() => handleNavigate("OPEN")} value="OPEN">
+    <TabsList className="w-fit h-[40px]">
+      <TabsTrigger
+        className="h-[32px]"
+        onClick={() => handleNavigate("OPEN")}
+        value="OPEN"
+      >
         Open ({allTasks.filter((task) => task.status === "OPEN").length})
       </TabsTrigger>
       <TabsTrigger
+        className="h-[32px]"
         onClick={() => handleNavigate("IN_PROGRESS")}
         value="IN_PROGRESS"
       >
         In Progress (
         {allTasks.filter((task) => task.status === "IN_PROGRESS").length})
       </TabsTrigger>
-      <TabsTrigger onClick={() => handleNavigate("CLOSED")} value="CLOSED">
+      <TabsTrigger
+        className="h-[32px]"
+        onClick={() => handleNavigate("CLOSED")}
+        value="CLOSED"
+      >
         Closed ({allTasks.filter((task) => task.status === "CLOSED").length})
       </TabsTrigger>
     </TabsList>
